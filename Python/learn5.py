e1db91f954f2b458da5907ea072401e6ce10918c
SrivastavaD <sdevansh296@gmail.com>
2026-05-19 13:53:12 +0530

    decorators and generators in python

--- Python/learn5.py
+++ Python/learn5.py
@@ -31,12 +31,51 @@
 # print(unique_spices)
 
 # generators in python
-def serve_chai():
-    yield "cup1: masala chai"
-    yield "cup2:ginger chao"
-    yield "cup3: elachi chai"
+# def serve_chai():
+#     yield "cup1: masala chai"
+#     yield "cup2:ginger chao"
+#     yield "cup3: elachi chai"
 
-    stall = serve_chai()
+#     stall = serve_chai()
 
-    for cup in stall:
-        print(cup)
+#     for cup in stall:
+#         print(cup)
+
+# def infinite_chai():
+#     count = 1
+#     while True:
+#         yield f"refil #{count}"
+#         count += 1
+# refill = infinite_chai()
+# for _ in range(3):
+#     print(next(refill))        
+
+# # decorators in python
+# def my_decorator(func):
+#     def wrapper():
+#         print("before function runs")
+#         func()
+#         print("after function runs")
+#     return wrapper
+
+# @my_decorator
+# def greet():
+#     print("Hello from decorated function!")   
+
+# greet()     
+
+from functools import wraps
+def log_activity(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"calling:{func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"finished: {func.__name__}")
+        return result
+    return wrapper
+
+@log_activity
+def brew_chai(type):
+    print(f"brewing {type} chai")
+
+brew_chai("masala")    
